0fe7e853bb4c68c2b82d87dc7b2b426fd7c4e509
MaulanaMLG <104117710+MaulanaMLG@users.noreply.github.com>
2022-11-20 09:51:13 +0800

    mengkoding button hapus & keluar

--- src/latihan3_maulana_5e/PerhitunganJumlahHari.java
+++ src/latihan3_maulana_5e/PerhitunganJumlahHari.java
@@ -43,8 +43,8 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
         jPanel4 = new javax.swing.JPanel();
         BtnHitung = new javax.swing.JButton();
         BtnSimpan = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        BtnHapus = new javax.swing.JButton();
+        BtnKeluar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -134,11 +134,21 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton3.setText("Hapus");
+        BtnHapus.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        BtnHapus.setText("Hapus");
+        BtnHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnHapusActionPerformed(evt);
+            }
+        });
 
-        jButton4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jButton4.setText("Keluar");
+        BtnKeluar.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        BtnKeluar.setText("Keluar");
+        BtnKeluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnKeluarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -147,12 +157,12 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(BtnHapus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(BtnHitung, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 109, Short.MAX_VALUE)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(BtnSimpan, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(BtnKeluar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel4Layout.setVerticalGroup(
@@ -164,8 +174,8 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
                     .addComponent(BtnSimpan))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4))
+                    .addComponent(BtnHapus)
+                    .addComponent(BtnKeluar))
                 .addContainerGap())
         );
 
@@ -255,6 +265,18 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_BtnSimpanActionPerformed
 
+    private void BtnHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnHapusActionPerformed
+        // TODO add your handling code here:
+        jumlahLabel.setText("Jumlah hari adalah ");
+        tahunTF.setText("");
+        bulanComboBox.setSelectedItem("Januari");
+    }//GEN-LAST:event_BtnHapusActionPerformed
+
+    private void BtnKeluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnKeluarActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_BtnKeluarActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -291,11 +313,11 @@ public class PerhitunganJumlahHari extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnHapus;
     private javax.swing.JButton BtnHitung;
+    private javax.swing.JButton BtnKeluar;
     private javax.swing.JButton BtnSimpan;
     private javax.swing.JComboBox<String> bulanComboBox;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
